b60d2a2439b7fc15aab40ab0d22f41dc7b408f98
baljinnyam.html@gmail.com <baljinnyam.html@gmail.com>
2023-03-24 01:31:18 +0800

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import NavBar from './components/NavBar';
 
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import { useEffect, useState } from 'react';
 import Signin from './components/Login/Signin';
 import Signup from './components/Login/Signup';
-
-
-
-
+import SigninSuccess from './components/Login/SigninSuccess';
+import Home from './components/pages/Home';
 
 
 function App() {
@@ -26,14 +24,27 @@ function App() {
 
   if(!me){
     return(
+      <>
       <Routes>
-        <Route path="/signin" element={<Signin/>} />
+        <Route path="/" element={<Signin/>} />
+        <Route
+          path="/signin/success"
+          element={<SigninSuccess setMe={setMe} />}
+        />
         <Route path="/signup" element={<Signup/>}/>
       </Routes>
+      </>
     )
   }
 
-  return <><NavBar/></>
+  return (
+    <>
+    <NavBar  />
+    <Routes>
+      <Route path='/home' element={<Home/>}/>
+    </Routes>
+    </>
+  )
 }
 
 export default App;
